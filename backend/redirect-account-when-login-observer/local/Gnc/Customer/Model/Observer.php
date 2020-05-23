aeb5912859e09dd19ae90d1744704f68a1270f56
fco edson aguiar <fcoedsonaguiar@gmail.com>
2020-05-23 12:50:41 -0300

    adicionando condicao quando cliente fizer login

--- backend/redirect-account-when-login-observer/local/Gnc/Customer/Model/Observer.php
+++ backend/redirect-account-when-login-observer/local/Gnc/Customer/Model/Observer.php
@@ -3,6 +3,9 @@ class Gnc_Customer_Model_Observer
 {
     public function customerLogin()
     {
-        return Mage::getSingleton('customer/session')->setBeforeAuthUrl(Mage::helper('checkout/cart')->getCartUrl());
+	if (Mage::getSingleton('customer/session')->isLoggedIn())
+	{
+            return Mage::getSingleton('customer/session')->setBeforeAuthUrl(Mage::helper('checkout/cart')->getCartUrl());
+	}
     }
 }
